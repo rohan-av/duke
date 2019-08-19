f33231bca1777da3ec651ee5d43d2186a60305cd
Rohan Arya Varma <rohanaryavarma@gmail.com>
2019-08-20 00:52:58 +0800

    Complete Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Duke {
+    private static ArrayList<String> list = new ArrayList<>();
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -8,9 +12,9 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo + wrap("What can I do for you?\n"));
-        echo();
+        handleCommand();
     }
-    private static void echo(){
+    private static void handleCommand(){
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()){
             String command = scanner.nextLine();
@@ -18,13 +22,30 @@ public class Duke {
                 System.out.println(wrap("Bye. Hope to see you again soon!"));
                 break;
             }
-            System.out.println(wrap(command));
+            if (command.equals("list")){
+                System.out.println(wrap(formatList(list)));
+            }
+            else {
+                list.add(command);
+                System.out.println(wrap("added: " + command));
+            }
         }
     }
 
     private static String wrap(String content){
-        return ("\n__________________________\n"
+        return ("\n_____________________________\n"
                 + content
-                + "\n__________________________\n");
+                + "\n_____________________________\n");
+    }
+
+    private static String formatList(ArrayList<String> list){
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i<list.size(); i++){
+            result.append(i + 1).append(". ").append(list.get(i));
+            if (i != list.size()-1){
+                result.append("\n");
+            }
+        }
+        return result.toString();
     }
 }
