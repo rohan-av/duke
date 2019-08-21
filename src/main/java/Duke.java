eb8ffc624f4dd54b15bd9a0428cb7dd70b7648b1
Rohan Arya Varma <rohanaryavarma@gmail.com>
2019-08-21 23:53:27 +0800

    Complete Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,8 @@
 import java.util.ArrayList;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class Duke {
-    private static ArrayList<String> list = new ArrayList<>();
+    private static ArrayList<Task> list = new ArrayList<>();
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -22,11 +21,24 @@ public class Duke {
                 System.out.println(wrap("Bye. Hope to see you again soon!"));
                 break;
             }
-            if (command.equals("list")){
-                System.out.println(wrap(formatList(list)));
+            else if (command.equals("list")){
+                System.out.println(formatList(list));
+            }
+            else if (command.length() >= 5 && command.substring(0,4).equals("done")){
+                int index = Integer.parseInt(command.substring(5));
+                if (list.size() == 0){
+                    System.out.println("List is empty! Please try again.");
+                }
+                if (index > list.size() || index < 1){
+                    System.out.println("Invalid index! Please try again.");
+                }
+                else {
+                    list.get(index - 1).setDone();
+                    System.out.println(formatDone(list, index));
+                }
             }
             else {
-                list.add(command);
+                list.add(new Task(command));
                 System.out.println(wrap("added: " + command));
             }
         }
@@ -38,14 +50,28 @@ public class Duke {
                 + "\n_____________________________\n");
     }
 
-    private static String formatList(ArrayList<String> list){
+    private static String formatList(ArrayList<Task> list){
         StringBuilder result = new StringBuilder();
         for (int i = 0; i<list.size(); i++){
-            result.append(i + 1).append(". ").append(list.get(i));
+            result.append(i + 1)
+                    .append(". [")
+                    .append(list.get(i).getStatusIcon())
+                    .append("] ")
+                    .append(list.get(i).getDescription());
             if (i != list.size()-1){
                 result.append("\n");
             }
         }
-        return result.toString();
+        return wrap(result.toString());
+    }
+
+    private static String formatDone(ArrayList<Task> list, int index){
+        String result = "Nice! I've marked this task as done:\n  " +
+                "[" +
+                list.get(index - 1).getStatusIcon() +
+                "] " +
+                list.get(index - 1).getDescription() +
+                "\n";
+        return wrap(result);
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "✓" : "✘"); //return tick or X symbols
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public void setDone() {
+        this.isDone = true;
+    }
+}
