e8b3f888b4743c567f13989326267bb6c98130f4
Rohan Arya Varma <rohanaryavarma@gmail.com>
2019-08-28 03:44:19 +0800

    Complete Level-7

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,5 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -29,46 +29,55 @@ public class Duke {
         handleCommand();
     }
 
-    private static void handleCommand() throws DukeException {
+    private static void handleCommand() {
         Scanner scanner = new Scanner(System.in);
         try {
-            while (scanner.hasNext()) {
-                String command = scanner.nextLine();
-                if (command.equals("bye")) {
-                    //to terminate the program
-                    System.out.println(wrap("Bye. Hope to see you again soon!"));
-                    break;
-                } else if (command.equals("list")) {
-                    //to print out the list
-                    System.out.println(formatList(list));
-                } else if (command.length() >= 6 && command.substring(0, 4).equals("done")) {
-                    //to mark tasks as done
-                    int index = Integer.parseInt(command.substring(5));
-                    if (list.size() == 0) {
-                        System.out.println("List is empty! Please try again.");
-                    }
-                    if (index > list.size() || index < 1) {
-                        System.out.println("Invalid index! Please try again.");
-                    } else {
-                        list.get(index - 1).setDone();
-                        updateFile();
-                        System.out.println(formatDone(list, index));
-                    }
-                } else {
-                    //to handle regular commands
-                    addTask(command);
-                    updateFile();
-                }
-            }
+            loadList();
         } catch (DukeException e) {
             System.out.println(wrap(e.getMessage()));
+        }
+        while (scanner.hasNext()) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                //to terminate the program
+                System.out.println(wrap("Bye. Hope to see you again soon!"));
+                break;
+            } else if (command.equals("list")) {
+                //to print out the list
+                System.out.println(formatList(list));
+            } else if (command.length() >= 6 && command.substring(0, 4).equals("done")) {
+                //to mark tasks as done
+                int index = Integer.parseInt(command.substring(5));
+                if (list.size() == 0) {
+                    System.out.println("List is empty! Please try again.");
+                }
+                if (index > list.size() || index < 1) {
+                    System.out.println("Invalid index! Please try again.");
+                } else {
+                    list.get(index - 1).setDone();
+                    try {
+                        updateFile();
+                    } catch (Exception e){
+                        System.out.println("OOPS!!! An IO exception has occurred.");
+                    }
+                    System.out.println(formatDone(list, index));
+                }
+            } else {
+                //to handle regular commands
+                try {
+                    addTask(command);
+                    updateFile();
+                } catch (DukeException e){
+                    System.out.println(wrap(e.getMessage()));
+                }
+            }
         }
     }
 
     private static String wrap(String content) {
-        return ("\n_____________________________\n"
+        return ("\n__________________________________\n"
                 + content
-                + "\n_____________________________\n");
+                + "\n__________________________________\n");
     }
 
     private static String formatList(ArrayList<Task> list) {
@@ -119,6 +128,67 @@ public class Duke {
         try {
             Files.write(file, tasks, StandardCharsets.UTF_8);
         } catch (IOException e) {
+            throw new DukeException("","io");
+        }
+    }
+
+    private static ArrayList<String> readFile() throws DukeException {
+        // reads file and returns an ArrayList of lines
+        ArrayList<String> result = new ArrayList<>();
+        try (BufferedReader br = Files.newBufferedReader(Paths.get("data", "todo_list.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null){
+                result.add(line);
+            }
+        } catch (Exception e) {
+            throw new DukeException("", "io");
+        }
+        return result;
+    }
+
+    private static void loadList() throws DukeException {
+        // loads data into list
+        ArrayList<String> data = readFile();
+        for (String line: data) convertString(line);
+    }
+
+    private static void convertString(String s) throws DukeException {
+        // converts a line (String) into a task
+        try {
+            String type = s.substring(1,2); // T, D or E
+            boolean isDone = s.substring(4,5).equals("v");
+            String description;
+            String addendum;
+            switch (type) {
+                case "T":
+                    description = s.substring(7);
+                    ToDo todo = new ToDo(description);
+                    if (isDone) todo.setDone();
+                    list.add(todo);
+                    break;
+                case "E": {
+                    String[] sections = s.substring(7).split("at:");
+                    description = sections[0].substring(0, sections[0].length() - 2);
+                    addendum = sections[1].substring(1, sections[1].length() - 1);
+                    Event event = new Event(description, addendum);
+                    if (isDone) event.setDone();
+                    list.add(event);
+                    break;
+                }
+                case "D": {
+                    String[] sections = s.substring(7).split("by:");
+                    description = sections[0].substring(0, sections[0].length() - 2);
+                    addendum = sections[1].substring(1, sections[1].length() - 1);
+                    Deadline deadline = new Deadline(description, addendum);
+                    if (isDone) deadline.setDone();
+                    list.add(deadline);
+                    break;
+                }
+                default:
+                    throw new DukeException("","io");
+            }
+
+        } catch (Exception e) {
             throw new DukeException("","io");
         }
     }
